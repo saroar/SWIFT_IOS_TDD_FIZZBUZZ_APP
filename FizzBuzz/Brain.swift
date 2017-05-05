66ddafcbf69575fe686762e079d2fd8ef4411976
saroar <saroar9@gmail.com>
2017-05-05 10:33:08 +0300

    all test passed for fizzbuzz

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -10,6 +10,25 @@ import Foundation
 
 class Brain {
     func isDividedByThree(number: Int) -> Bool {
-        return true
+        return isDivisibleBy(divisor: 3, number: number)
+    }
+    
+    func isDivisibleByFive(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 5, number: number)
+    }
+    
+    func isDivisibleByFiftheen(number: Int) -> Bool {
+        return isDivisibleBy(divisor: 15, number: number)
+    }
+    
+    func isDivisibleBy(divisor: Int, number: Int) -> Bool {
+        return number % divisor == 0
+    }
+    
+    func check(number: Int) -> String {
+        if isDivisibleByFiftheen(number: number)  { return "fizzbuzz"  }
+        if isDividedByThree(number: number)      { return "fizz"      }
+        if isDivisibleByFive(number: number)      { return "buzz"      }
+        else                                      { return "\(number)" }
     }
 }
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -20,12 +20,45 @@ class BrainTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
+    let brain = Brain()
     
     func testDividedByThree() {
-        let brain = Brain()
+
         let result = brain.isDividedByThree(number: 3)
         
         XCTAssertEqual(result, true)
+    }
+    
+    func testIsDivisibleByFive() {
+
+        let result = brain.isDivisibleByFive(number: 5)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsDivisibleByFiftheen() {
+        
+        let result = brain.isDivisibleByFiftheen(number: 15)
+        XCTAssertEqual(result, true)
+    }
+    
+    func testSayFizz() {
+        let result = brain.check(number: 3)
+        XCTAssertEqual(result, "fizz")
+    }
+    
+    func testSayBuzz() {
+        let result = brain.check(number: 5)
+        XCTAssertEqual(result, "buzz")
+    }
+    
+    func testSayFizzBuzz() {
+        let result = brain.check(number: 15)
+        XCTAssertEqual(result, "fizzbuzz")
+    }
+    
+    func testSayNumber() {
+        let result = brain.check(number: 1)
+        XCTAssertEqual(result, "1")
     }
     
 }
